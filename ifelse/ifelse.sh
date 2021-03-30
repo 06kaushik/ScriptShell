700dac1337314eb5e9b188914ec5e58a5d7f97b5
06kaushik <himanshu06kaushik@gmail.com>
2021-03-30 10:40:53 +0530

    efelse statement

---
+++ ifelse/ifelse.sh
@@ -0,0 +1,38 @@
+#!/bin/bash -x
+
+isPresent=1
+randomCheck=$((RANDOM%2))
+if [ $isPresent -eq $randomCheck ]
+then 
+     echo "Employee is Present"
+else
+     echo "Employee is Abscent"
+fi
+
+isPresent=1
+randomCheck=$((RANDOM%2))
+if [ $isPresent -eq $randomCheck ]
+then 
+     empRatePerHr=20
+     empHrs=8
+     salary=$(($empHrs*$empRatePerHr))
+else
+   salary=0
+fi
+
+
+isPartTime=1
+isFullTime=2
+empRatePerHr=20
+randomCheck=$((RANDOM))
+
+if [ $isFullTime -eq $randomCheck ]
+then 
+      empHrs=8
+elif [ $isPartTime -eq $randomCheck ]
+then
+     empHrs=4
+else
+    empHrs=0
+fi
+salary=$(($empHrs*$empRatePerHr))
